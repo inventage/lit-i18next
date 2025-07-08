7393f80fa949524b3cc96a2bf5f0a8c80165ed60
Peter Siska <63866+peschee@users.noreply.github.com>
2025-07-08 21:28:20 +0200

    test: add localized decorator test

--- test/localized-decorator.spec.ts
+++ test/localized-decorator.spec.ts
@@ -1,17 +1,24 @@
 import { expect, fixture, html } from '@open-wc/testing';
 import { customElement } from 'lit/decorators/custom-element.js';
 import { LitElement } from 'lit';
+
+import { updateWhenLocaleChanges } from '../src/localized-controller';
 import { i18next, localized, t } from '../src/lit-i18next';
 
 @localized()
 @customElement('test-element')
 class TestElement extends LitElement {
+  constructor() {
+    super();
+    updateWhenLocaleChanges(this);
+  }
+
   override render() {
     return html`${t('foo')}`;
   }
 }
 
-describe('localized-controller', () => {
+describe('@localized', () => {
   before(async () => {
     await i18next.init({
       lng: 'en',
