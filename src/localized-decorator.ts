b5c461d79cbead3c3c9db06746afb73858657dea
Peter Siska <63866+peschee@users.noreply.github.com>
2025-07-08 17:06:37 +0200

    refactor: bump versions

--- src/localized-decorator.ts
+++ src/localized-decorator.ts
@@ -1,6 +1,7 @@
 /**
  * @license
  * Copyright 2021 Weave B.V.
+ * Copyright 2025 Inventage AG
  * SPDX-License-Identifier: MIT License
  */
 
--- src/localized-helpers.ts
+++ src/localized-helpers.ts
@@ -1,11 +1,14 @@
 /**
  * @license
  * Copyright 2021 Weave B.V.
+ * Copyright 2025 Inventage AG
  * SPDX-License-Identifier: MIT License
  */
 
 import type { TFunction } from 'i18next';
 import { default as i18next } from 'i18next';
 
+// noinspection JSUnusedGlobalSymbols
 export { default as i18next } from 'i18next';
+// noinspection JSUnusedGlobalSymbols
 export const t: TFunction = i18next.t.bind(i18next);
